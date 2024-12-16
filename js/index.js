9a252efda960d20bdab47e790b3410ea64df8a31
Aliaksei Karzhou <alexius90e@gmail.com>
2024-12-16 15:45:10 +0300

    feat: added header location

--- js/index.js
+++ js/index.js
@@ -22,11 +22,26 @@ const headerBurger = document.querySelector('.header__burger');
 
 if (headerBurger) {
   headerBurger.addEventListener('click', (event) => {
-    console.log('header__burger')
     const isLayout = event.currentTarget === event.target;
     const isButton = event.target.classList.contains('header__burger-button');
 
     if (isLayout) headerBurger.classList.remove('active');
     if (isButton) headerBurger.classList.toggle('active');
+  });
+}
+
+const headerLocation = document.querySelector('.header__location');
+
+if (headerLocation) {
+  headerLocation.addEventListener('click', (event) => {
+    const isLayout = event.currentTarget === event.target;
+    const isButtonLocation = event.target.classList.contains('header__location-button');
+    const isButtonOk = event.target.classList.contains('header__location-modal-buttons-ok');
+    const isButtonChoose = event.target.classList.contains('header__location-modal-buttons-choose');
+
+    if (isLayout) headerLocation.classList.remove('active');
+    if (isButtonLocation) headerLocation.classList.add('active');
+    if (isButtonOk) headerLocation.classList.remove('active');
+    if (isButtonChoose) headerLocation.classList.remove('active');
   });
 }
